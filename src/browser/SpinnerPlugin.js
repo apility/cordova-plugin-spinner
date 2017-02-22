d548762e0955b6b36b2a971f5bf819094880c4cc
Erik Djupvik <erikdju@gmail.com>
2017-02-22 15:18:06 +0100

    Add spinner element to DOM in browser

--- src/browser/SpinnerPlugin.js
+++ src/browser/SpinnerPlugin.js
@@ -1,10 +1,22 @@
+let el = document.createElement('div')
+el.classList.add('loading-indicator')
+document.body.appendChild(el)
+
 module.exports = {
     activityStart: function (labelText, options, successCallback, failureCallback) {
+        if(!el.classList.contains('loading')) {
+            el.classList.add('loading')
+        }
+
         if(typeof successCallback == 'function'){
             successCallback();
         }
     },
     activityStop: function(successCallback, failureCallback) {
+        if(el.classList.contains('loading')) {
+            el.classList.remove('loading')
+        }
+
         if(typeof successCallback == 'function'){
             successCallback();
         }
